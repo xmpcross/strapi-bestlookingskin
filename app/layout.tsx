16e333f19439b4a293a6ab9f02bc2f964ed52230
Vampy <kryptok112@gmail.com>
2026-09-24 19:15:23 +0800

    feat(analytics): add Ahrefs analytics script to head

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,6 +60,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2867376862905050"
           crossOrigin="anonymous"
         />
+        <script
+          async
+          src="https://analytics.ahrefs.com/analytics.js"
+          data-key="xYkdnOIzip7zvZyecNubIA"
+        />
       </head>
       <body data-testid="app-shell">
         <div id="top" />
